631ea292de4e389f8cdddd9efc1f16deac7f6a46
Matúš Kačmár <ma.kacmar@akademiasovy.sk>
2015-11-07 22:40:30 +0100

    Temporary registration form

--- pages/registration.php
+++ pages/registration.php
@@ -1,1 +1,14 @@
-<p>Registration</p>
+<div class="frame-container registration">
+    <form action="" method="POST">
+        <ul class="registration-container">
+            <li class="registration-item"><label for="name">First name:</label><input type="text" name="name" placeholder="First name..."></li>
+            <li class="registration-item"><label for="last-name">Last name:</label><input type="text" name="last-name" placeholder="Last name..."></li>
+            <li class="registration-item"><label for="mail">E-mail:</label><input type="email" name="mail" placeholder="johnSmith@mail.com"></li>
+            <li class="registration-item"><label for="password">Password:</label><input type="password" name="password" placeholder="*******"></li>
+            <li class="registration-item"><label for="adress">Adress:</label><input type="text" name="addres" placeholder="Adress..."></li>
+            <li class="registration-item"><label for="city">City:</label><input type="text" name="city" placeholder="City..."></li>
+            <li class="registration-item"><label for="state">State:</label><input type="text" name="state" placeholder="State..."></li>
+            <li class="registration-item"><input type="submit" value="Register"></li>
+        </ul>
+    </form>
+</div>
