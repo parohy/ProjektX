338767cbb3eadf40a3d31f1001b7951df99be657
Matúš Kačmár <ma.kacmar@akademiasovy.sk>
2015-11-08 14:32:50 +0100

    Login form styling
    Little changes to login form styling

--- index.php
+++ index.php
@@ -129,7 +129,8 @@
                             <li class="login-item"><input type="text" name="usermail" placeholder="Email..." maxlength="40"><span class="error"><?php echo $nameErr?></span></li>
                             <li class="login-item"><label for="usermail">Password:</label></li>
                             <li class="login-item"><input type="password" name="password" placeholder="Pasword..." maxlength="30"><span class="error"><?php echo $passErr?></span></li>
-                            <li class="login-item">Do you want to stay logged in ? <input type="checkbox" name="stayLoggedin" value="true"></li>
+                            <li class="login-item"><span class="login-dialogue">Do you want to stay logged in ?</span><input type="checkbox" name="stayLoggedin" value="true"></li>
+                            <li class="login-item"><a href="?page=forgottenPass">Forgot your password ?</a></li>
                             <li class="login-item"><a href="?page=registration">Not registered yet ?</a></li>
                             <li class="login-item"><input type="submit" value="Log in"></li>
                         </ul>
