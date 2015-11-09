458bfeddbd73aa8c0fc83dca6bcfcb71270a5c8d
Matúš Kačmár <ma.kacmar@akademiasovy.sk>
2015-11-09 10:36:14 +0100

    Slider

--- pages/slider.php
+++ pages/slider.php
@@ -1,14 +1,28 @@
 <div class="slider">
-        <a href="#" class="left-arrow"><</a>
-        <div class="slider-content">
-            slide 1
-        </div>
+    <script type="text/javascript">
+        <!-->
+        var image1=new Image()
+        image1.src="img/slider/slide1.jpg"
+        var image2=new Image()
+        image2.src="img/slider/slide2.jpg"
+        var image3=new Image()
+        image3.src="img/slider/slide3.jpg"
+        //-->
+    </script>
 
-        <div class="slider-content">
-            slide 2
-        </div>
-
-        <div class="slider-content">
-            slide 3
-        </div>
+    <img src="slide1.jpg" name="slide" width="50%" height="100%">
+    <script type="text/javascript">
+        <!--
+        var step=1
+        function slideit(){
+            document.images.slide.src=eval("image"+step+".src");
+            if(step<3)
+                step++;
+            else
+                step=1;
+            setTimeout("slideit()",2500);
+        }
+        slideit();
+        //-->
+    </script>
 </div>
