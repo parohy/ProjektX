b0b34e97752eb413b70118f7fc5e5849f6737454
dominikkolesar <dominik.kolesar@akademiasovy.sk>
2015-11-12 00:49:07 +0100

    main-nav-menu

--- js/menuScript.js
+++ js/menuScript.js
@@ -2,10 +2,31 @@
  * Created by Matúš on 3. 11. 2015.
  */
 
-$(function(){
-    $(".subnav-container").hide();
 
-    $(".nav-button").click(function(){
-        $(this).siblings(".subnav-container").toggle("fast");
+
+
+
+
+$(function(){
+    $(".subnav-container").hide(0);
+
+    $(".nav-button").mouseover(function(){
+        $(this).siblings(".subnav-container").toggle("fast").delay(1000);
+
+
     });
+
+
+
+
+});
+
+$(function(){
+    $(".subnav-container").mouseleave(function(){
+
+        $(".subnav-container").hide(0);
+
+
+    });
+
 });
