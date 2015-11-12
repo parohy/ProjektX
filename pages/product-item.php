dea60f06cc3c77192e431732fb77a1d0295f0539
dominikkolesar <dominik.kolesar@akademiasovy.sk>
2015-11-12 22:05:14 +0100

    Formular pre product-iteam /ulozeny v pages/product-item.php

---
+++ pages/product-item.php
@@ -0,0 +1,94 @@
+
+
+<!doctype html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+
+    <link rel="stylesheet" type="text/css" href="css/form-product-item.css">
+    <title>product-item</title>
+</head>
+
+<body>
+
+    <div id="product-item">
+        <table id="table">
+            <form name="pruduct-item-formular" action="" method="get">
+
+
+             <tr>
+
+                 <td> <span class="input-name">Product Id:</span> </td> <td><input class="input" type="text" name="productid" id="productid" size="20"></td>
+
+             </tr>
+
+              <tr>
+
+                <td> <span class="input-name">Elektronics</td> <td><input class="input" type="checkbox" name="categoryname" id="categoryname" ></td>
+
+              </tr>
+
+              <tr>
+
+                 <td><span class="input-name"> Nootebooks and PC</td>  <td><input class="input" type="checkbox" name="categoryname" id="categoryname" ></td>
+               </tr>
+
+              <tr>
+                <td><span class="input-name">Mobile </td> <td> <input class="input" type="checkbox" name="categoryname" id="categoryname" > </td>
+              <tr>
+
+                  <td><span class="input-name">Small device</td>  <td><input class="input" type="checkbox" name="categoryname" id="categoryname" > </td>
+                     </tr>
+
+              <tr>
+
+                  <td><span class="input-name">Amount </td> <td><input class="input" type="text" name="amount" id="amount" size="20"> </td>
+                      </tr>
+
+              <tr>
+                  <td><span class="input-name">Description </td> <td> <textarea class="input" type="text" name="name" id="name" size="200"></textarea></td>
+                    </tr>
+
+              <tr>
+                  <td><span class="input-name">Price </td>  <td><input class="input" type="text" name="price" id="price" size="20"></td>
+                    </tr>
+
+              <tr>
+                  <td><span class="input-name">Brand </td>  <td><input class="input" type="text" name="brand" id="brand" size="20"></td>
+                    </tr>
+
+              <tr>
+                  <td><span class="input-name"> Description</td> <td> <textarea class="input" rows="5" cols="20" name="descriptoin" id="description" size="20">
+                    </textarea></td>
+                    </tr>
+
+              <tr>
+                  <td><span class="input-name">Viewamount </td> <td><input class="input" type="text" name="viewamount" id="viewamount" size="20"></td>
+                    </tr>
+
+              <tr>
+                  <td><span class="input-name">Picture </td> <td><input class="input" type="file" name="imagepath" id="imagepath"></td>
+                    </tr>
+                <tr>
+                  <td><input type="submit" name="sent" id="sent"><td>
+                </tr>
+
+             </form>
+
+        </table>
+
+
+    </div>
+
+
+
+
+
+
+
+</body>
+
+</html>
+
+
