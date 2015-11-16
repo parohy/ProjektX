2f13a014804a05067a948ae137fa5bd3a41ca36a
Matúš Kačmár <matus.kacmar@akademiasovy.sk>
2015-11-16 16:59:01 +0100

    Registration validation

--- pages/registration.php
+++ pages/registration.php
@@ -6,15 +6,16 @@
     	session_start();
     }
     ?>
+  <script src="../ProjektX/js/regValidation.js"></script>
 	<form action="API/Relog.php?register=registration&id=1" method="POST">
 		<ul class="registration-container">
             <li>
                 <h4 class="headline">Registration</h4>
                 <ul>
-                    <li class="registration-item"><label for="name">First name:</label><input type="text" name="name" placeholder="First name..." maxlength="20" required></li>
-                    <li class="registration-item"><label for="last-name">Last name:</label><input type="text" name="last-name" placeholder="Last name..." maxlength="20" required></li>
-                    <li class="registration-item"><label for="mail">E-mail:</label><input type="email" name="mail" placeholder="johnSmith@mail.com" maxlength="40" required></li>
-                    <li class="registration-item"><label for="password">Password:</label><input type="password" name="password" placeholder="*******" maxlength="30" required></li>
+                    <li class="registration-item"><label for="name">First name:</label><input type="text" name="name" placeholder="First name..." maxlength="20" required id="first-name"></li>
+                    <li class="registration-item"><label for="last-name">Last name:</label><input type="text" name="last-name" placeholder="Last name..." maxlength="20" required id="last-name"></li>
+                    <li class="registration-item"><label for="mail">E-mail:</label><input type="email" name="mail" placeholder="johnSmith@mail.com" maxlength="40" required id="email"></li>
+                    <li class="registration-item"><label for="password">Password:</label><input type="password" name="password" placeholder="*******" maxlength="30" required id="password"></li>
                 </ul>
             </li>
             <li class="registration-item"><input type="submit" value="Register"></li>
