4072444ee15bf117bc41e6e8e48ee8bd0a7f42a6
Matúš Kačmár <matus.kacmar@akademiasovy.sk>
2015-11-23 14:11:55 +0100

    Nová fičura categories v product preview

--- pages/productPreview.php
+++ pages/productPreview.php
@@ -10,9 +10,29 @@ $handler->query("SELECT * FROM products WHERE productid = :id");
 $handler->bind(":id",$product);
 $result = $handler->singleRecord();
 
+/************************************ IMAGES *******************************************/
+
 $handler->query("SELECT * FROM images WHERE productid=:id");
 $handler->bind(":id",$product);
 $image = $handler->singleRecord();
+
+/************************************ CATEGORIES *******************************************/
+
+$handler->query("SELECT * FROM categories WHERE categoryid=:category");
+$handler->bind(":category",$result['categoryid']);
+$actualCategoryResult = $handler->singleRecord();
+
+$handler->query("SELECT * FROM categories WHERE categoryid=:category");
+$handler->bind(':category',$actualCategoryResult['parent']);
+$categoryResult = $handler->singleRecord();
+
+$handler->query("SELECT * FROM categories WHERE categoryid=:category");
+$handler->bind(':category',$categoryResult['parent']);
+$parentCategoryResult = $handler->singleRecord();
+
+$handler->endTransaction();
+
+/************************************ RATING *******************************************/
 
 if($result['sumofratings'] > 0 && $result['numofratings'] > 0) { // Calculate rating for given product
     $avgRating = round($result['sumofratings'] / $result['numofratings'], 1);
@@ -34,7 +54,15 @@ if ($result['amount'] > 5) {
     <div class="frame-content">
         <section>
             <article>
-                <header class="product-header"><h1><?php echo $result['name'];?></h1></header>
+                <header class="product-header">
+                    <h1><?php echo $result['name'];?></h1>
+                    <br>
+                    <?php
+                    if($parentCategoryResult['categoryid'] != 1) echo "<a href=\"?page=" . $parentCategoryResult['name'] . "&category=" . $parentCategoryResult['categoryid'] . "&view=10\">" . $parentCategoryResult['name'] . "</a>>";
+                    echo "<a href=\"?page=" . $categoryResult['name'] . "&category=" . $categoryResult['categoryid'] . "&view=10\">" . $categoryResult['name'] . "</a>>";
+                    echo "<a href=\"?page=" . $actualCategoryResult['name'] . "&category=" . $actualCategoryResult['categoryid'] . "&view=10\">" . $actualCategoryResult['name'] . "</a>";
+                    ?>
+                </header>
 
                 <div class="product-content">
                     <div class="product-image">
