c829b2db953f3f1aa5253a2afdd30765a164958c
Matúš Kačmár <matus.kacmar@akademiasovy.sk>
2015-12-01 20:49:48 +0100

    New design main page

---
+++ newDesign/index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width = device-width, initial-scale = 1">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="libraries/css/main.css">
+    <script src="libraries/js/jquery-1.11.3.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <header class="page-header">
+        <div class="top">
+            <div class="welcome">
+                <span class="welcome-label">Welcome User you can <strong class="welcome-buttons">Login</strong> or <strong class="welcome-buttons">Register</strong></span>
+            </div>
+
+            <div class="title">
+                <h1>VIATECH</h1>
+            </div>
+
+            <div class="search">
+                <span>SHOPPING CART: <span class="amount">0.00 EUR</span></span><br>
+                <input type="search" placeholder="SEARCH">
+            </div>
+        </div>
+
+        <div class="bottom">
+            <nav class="page-nav">
+                <a href="#">TV,AUDIO</a>
+                <a href="#">PC,TABLETS</a>
+                <a href="#">MOBILE PHONES</a>
+            </nav>
+        </div>
+    </header>
+
+    <main class="main">
+        <div class="slider">
+            <div id="left-arrow">
+                <
+            </div>
+
+            <div id="right-arrow">
+                >
+            </div>
+        </div>
+
+        <div class="tabs">
+            <div class="controls">
+                <a href="#" class="link">BEST SELLING <span class="arrow">&#8744;</span></a>
+                <a href="#" class="link">TOP RATED <span class="arrow">&#8744;</span></a>
+                <a href="#" class="link">NEW ARRIVALS <span class="arrow">&#8744;</span></a>
+            </div>
+            <div class="tab-content">
+                <div class="top-tab-content">
+                    <p>WE HAVE OVER 50 PRODUCTS IN OUR SHOP</p>
+                </div>
+
+                <div class="bottom-tab-content">
+                    <div class="product-item">
+                    </div>
+
+                    <div class="product-item">
+                    </div>
+
+                    <div class="product-item">
+                    </div>
+
+                    <div class="product-item">
+                    </div>
+
+                    <div class="product-item">
+                    </div>
+
+                    <div class="product-item">
+                    </div>
+
+                    <div class="product-item">
+                    </div>
+
+                    <div class="product-item">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer class="footer">
+    </footer>
+
+    <script src="libraries/js/pageScript.js"></script>
+</div>
+</body>
+</html>
---
+++ newDesign/libraries/js/pageScript.js
@@ -0,0 +1,7 @@
+$(function(){
+    $(".link").hover(function(){
+            $(this).addClass('link-hover');
+    }, function(){
+            $(this).removeClass('link-hover');
+    });
+});
