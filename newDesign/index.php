a19a165855626f46f9f4b6d44110139101f79d12
dominikkolesar <dominik.kolesar@akademiasovy.sk>
2015-12-02 09:05:10 +0100

    new design footer

--- newDesign/index.php
+++ newDesign/index.php
@@ -142,18 +142,33 @@
         </div>
     </main>
 
-    <footer class="footer">
-        <h3>CONNECT WITH US</h3>
-        <p>
-            <b>VIATECH</b>
-            <br>
-            <br>
-            Dneperská 1, Košice, Slovakia<br>
-            +421 902 095 588<br>
-            eleshopsk@gmail.com
-        </p>
-    </footer>
+        <footer class="footer">
+            <div class="contact-us">
+                <div class="foot-title">
+                    <div class="text">CONNECT WITH US</div>
+                </div>
 
+                <ul>
+                    <li>Viashop</li>
+                    <li>Dneperska 1,Kosice, Slovakia</li>
+                    <li>+421 902 095 588</li>
+                    <li>Viashopsk@gmail.com</li>
+                </ul>
+                <div class="social-icons">
+                    <img src="libraries/img/footer/fb.png">
+                    <img src="libraries/img/footer/g+.png">
+                    <img src="libraries/img/footer/li.png">
+                    <img src="libraries/img/footer/tw.png">
+                </div>
+            </div>
+            <div class="payment-icons">
+                <img src="libraries/img/footer/maestro.png">
+                <img src="libraries/img/footer/paypal.png">
+                <img src="libraries/img/footer/aexpress.png">
+                <img src="libraries/img/footer/mastercard.png">
+                <img src="libraries/img/footer/visa.png">
+            </div>
+        </footer>
     <script src="libraries/js/pageScript.js"></script>
 </div>
 </body>
