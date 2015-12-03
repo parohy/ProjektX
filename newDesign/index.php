bf5b94f88d2cbfec4873dafbc7e7d65b2b9fbafe
Matúš Kačmár <matus.kacmar@akademiasovy.sk>
2015-12-03 10:38:20 +0100

    Update

--- newDesign/index.php
+++ newDesign/index.php
@@ -13,7 +13,7 @@
     <header class="page-header">
         <div class="top">
             <div class="welcome">
-                <span class="welcome-label">Welcome User you can <strong class="welcome-buttons">Login</strong> or <strong class="welcome-buttons">Register</strong></span>
+                <span class="welcome-label"><img src="libraries/img/icons/user icon.png">Welcome User you can <strong class="welcome-buttons">Login</strong> or <strong class="welcome-buttons">Register</strong></span>
             </div>
 
             <div class="title">
@@ -21,7 +21,15 @@
             </div>
 
             <div class="search">
-                <span>SHOPPING CART: <span class="amount">0.00 EUR</span></span><br>
+                <div class="cart">
+                    <div class="cart-icon">
+                        <img src="libraries/img/icons/cart-icon.png">
+                    </div>
+                    <div class="cart-title">
+                        SHOPPING CART :<span class="amount">0.00 EUR</span>
+                    </div>
+                </div>
+                <br>
                 <input type="search" placeholder="SEARCH">
             </div>
         </div>
@@ -48,9 +56,10 @@
 
         <div class="tabs">
             <div class="controls">
-                <a href="#" class="link">BEST SELLING <span class="arrow">&#8744;</span></a>
-                <a href="#" class="link">TOP RATED <span class="arrow">&#8744;</span></a>
-                <a href="#" class="link">NEW ARRIVALS <span class="arrow">&#8744;</span></a>
+                <ul>
+                    <li><a href="#" class="link">BEST SELLING <span class="arrow">&#8744;</span></a></li>
+                    <li><a href="#" class="link">TOP RATED <span class="arrow">&#8744;</span></a></li>
+                    <li><a href="#" class="link">NEW ARRIVALS <span class="arrow">&#8744;</span></a></li>
             </div>
             <div class="tab-content">
                 <div class="top-tab-content">
@@ -58,85 +67,104 @@
                 </div>
 
                 <div class="bottom-tab-content">
-                    <div class="product-item">
+                    <div class="product-item  first-row">
                         <div class="product-photo">
                         </div>
 
                         <div class="product-description">
                             <hr class="product-line">
-                            <h3>PRODUCT</h3>
+                            <h3 class="product-name">PRODUCT</h3>
+                            <span class="price">$299.0</span>
+                            <a href="#" class="addToCart">Add to Cart</a>
                         </div>
                     </div>
 
-                    <div class="product-item">
+                    <div class="product-item second-row">
                         <div class="product-photo">
                         </div>
 
                         <div class="product-description">
                             <hr class="product-line">
-                            <h3>PRODUCT</h3>
+                            <h3 class="product-name">PRODUCT</h3>
+                            <span class="price">$299.0</span>
+                            <a href="#" class="addToCart">Add to Cart</a>
                         </div>
                     </div>
 
-                    <div class="product-item">
+                    <div class="product-item first-row">
                         <div class="product-photo">
                         </div>
 
                         <div class="product-description">
                             <hr class="product-line">
-                            <h3>PRODUCT</h3>
+                            <h3 class="product-name">PRODUCT</h3>
+                            <span class="price">$299.0</span>
+                            <a href="#" class="addToCart">Add to Cart</a>
                         </div>
                     </div>
 
-                    <div class="product-item">
+                    <div class="product-item second-row">
                         <div class="product-photo">
                         </div>
 
                         <div class="product-description">
                             <hr class="product-line">
-                            <h3>PRODUCT</h3>
+                            <h3 class="product-name">PRODUCT</h3>
+                            <span class="price">$299.0</span>
+                            <a href="#" class="addToCart">Add to Cart</a>
                         </div>
                     </div>
 
-                    <div class="product-item">
+                    <div class="product-item first-row">
                         <div class="product-photo">
                         </div>
 
                         <div class="product-description">
                             <hr class="product-line">
-                            <h3>PRODUCT</h3>
+                            <h3 class="product-name">PRODUCT</h3>
+                            <span class="price">$299.0</span>
+                            <a href="#" class="addToCart">Add to Cart</a>
                         </div>
                     </div>
 
-                    <div class="product-item">
+                    <div class="product-item second-row">
                         <div class="product-photo">
                         </div>
 
                         <div class="product-description">
                             <hr class="product-line">
-                            <h3>PRODUCT</h3>
+                            <h3 class="product-name">PRODUCT</h3>
+                            <span class="price">$299.0</span>
+                            <a href="#" class="addToCart">Add to Cart</a>
                         </div>
                     </div>
 
-                    <div class="product-item">
+                    <div class="product-item first-row">
                         <div class="product-photo">
                         </div>
 
                         <div class="product-description">
                             <hr class="product-line">
-                            <h3>PRODUCT</h3>
+                            <h3 class="product-name">PRODUCT</h3>
+                            <span class="price">$299.0</span>
+                            <a href="#" class="addToCart">Add to Cart</a>
                         </div>
                     </div>
 
-                    <div class="product-item">
+                    <div class="product-item second-row">
                         <div class="product-photo">
                         </div>
 
                         <div class="product-description">
                             <hr class="product-line">
-                            <h3>PRODUCT</h3>
+                            <h3 class="product-name">PRODUCT</h3>
+                            <span class="price">$299.0</span>
+                            <a href="#" class="addToCart">Add to Cart</a>
                         </div>
                     </div>
+                </div>
+                <div class="more">
+                    <a href="#">+ LOAD MORE PRODUCTS</a>
                 </div>
             </div>
         </div>
--- newDesign/libraries/js/pageScript.js
+++ newDesign/libraries/js/pageScript.js
@@ -1,7 +1,46 @@
-$(function(){
-    $(".link").hover(function(){
-            $(this).addClass('link-hover');
-    }, function(){
-            $(this).removeClass('link-hover');
+$(function() {
+
+    $(".addToCart").hide();
+
+    $(".link").hover(function() {
+        $(this).css("font-weight", "bold");
+    }, function() {
+        $(this).css("font-weight", "lighter");
+    });
+
+    $(".product-item.first-row").hover(function() {
+        $(this).stop(true, false).animate({
+            height: "470px"
+        });
+        $(this).next().stop(true, false).animate({
+            height: "370px"
+        });
+        $(this).children('.product-description').children('.addToCart').stop(true, false).slideDown();
+    }, function() {
+        $(this).stop(true, false).animate({
+            height: "424px"
+        });
+        $(this).next().stop(true, false).animate({
+            height: "424px"
+        });
+        $(this).children('.product-description').children('.addToCart').stop(true, false).slideUp();
+    });
+
+    $(".product-item.second-row").hover(function() {
+        $(this).stop(true, false).animate({
+            height: "470px"
+        });
+        $(this).prev().stop(true, false).animate({
+            height: "370px"
+        });
+        $(this).children('.product-description').children('.addToCart').stop(true, false).slideDown();
+    }, function() {
+        $(this).stop(true, false).animate({
+            height: "424px"
+        });
+        $(this).prev().stop(true, false).animate({
+            height: "424px"
+        });
+        $(this).children('.product-description').children('.addToCart').stop(true, false).slideUp();
     });
 });
