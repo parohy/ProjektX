38f7cb3ec407efba339a619e1cc8e31c682d958f
dominikkolesar <dominik.kolesar@akademiasovy.sk>
2015-12-05 13:50:16 +0100

    reg-acc page, mam tam este nejake mensie cc chyby ale opravim ich.

---
+++ newDesign/view/pages/reg-acc.php
@@ -0,0 +1,35 @@
+
+<link rel="stylesheet" type="text/css" href="../css/reg-acc.css">
+
+
+
+
+    <div class="main">
+        <div class="settings">
+            <h1>ACCOUNT SETTINGS</h1>
+        </div>
+
+        <div class="clearfix">
+            <div class="left">
+
+                <ul class="nav">
+                     <li><a href="#" class="link">LOGIN</a></li>
+                     <li><a href="#" class="link">REGISTER</a></li>
+                     <li><a href="#" class="link">RECOVER PASSWORD</a></li>
+                 </ul>
+
+             </div>
+
+             <div class="right">
+                 <form action="" method="get">
+                     <ul>
+                         <li><input class="input" type="text" placeholder="name" name="name"> </li>
+                         <li><input class="input" type="password" placeholder="password" name="password"></li>
+                         <li><input  type="submit" value="Submit"></li>
+                     </ul>
+
+             </div>
+        </div>
+    </div>
+
+
