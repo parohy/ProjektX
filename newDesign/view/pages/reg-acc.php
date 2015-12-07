643967b26ffffc41c6156cc719b91848d024f380
dominikkolesar <dominik.kolesar@akademiasovy.sk>
2015-12-07 10:57:41 +0100

    reg-acc account seting oprava

--- newDesign/view/pages/reg-acc.php
+++ newDesign/view/pages/reg-acc.php
@@ -4,30 +4,56 @@
 
 
 
-    <div class="reg-formular">
-        <div class="title-page">
-            <h1>ACCOUNT SETTINGS</h1>
+
+
+<link rel="stylesheet" href="tab2.css">
+
+<div id="tab">
+    <div class="title-page">
+        <h1>ACCOUNT SETTINGS</h1>
+    </div>
+    <div id="formulars"class="group">
+        <div id="menu-content">
+            <ul id="menu">
+                <li><a href="#tab1">REGISTRATION</a></li>
+                <li><a href="#tab2">LOGIN</a></li>
+                <li><a href="#tab3">RECOVER PASSWORD</a></li>
+            </ul>
         </div>
 
-        <div id="reg-content" class="group">
-            <div class="left-side">
+        <div id="all-tab">
+            <div id="tab1" class="tab-content">
+                REGISTER
+                <form class="form"action="" method="get">
+                    <ul>
+                        <li><input class="input" type="text" placeholder="name" name="name"> </li>
+                        <li><input class="input" type="password" placeholder="password" name="password"></li>
+                        <li><input class="submit-button"  type="submit" value="REGISTER"></li>
+                    </ul>
+                </form>
+            </div>
+            <div id="tab2" class="tab-content">
+                LOGIN
+                <form class="form" action="" method="get">
+                    <ul>
+                        <li><input class="input" type="text" placeholder="name" name="name"> </li>
+                        <li><input class="input" type="password" placeholder="password" name="password"></li>
+                        <li><input class="submit-button"  type="submit" value="LOGIN"></li>
+                    </ul>
+                </form>
 
-                <ul class="nav-bar">
-                     <li><a href="#" class="formular-link">LOGIN</a></li>
-                     <li><a href="#" class="formular-link">REGISTER</a></li>
-                     <li><a href="#" class="formular-link">RECOVER PASSWORD</a></li>
-                 </ul>
+            </div>
+            <div id="tab3" class="tab-content">
+                recovery pasword
+                <form class="form" action="" method="get">
+                    <ul>
+                        <li><input class="input" type="text" placeholder="name" name="name"> </li>
+                        <li><input class="input" type="password" placeholder="password" name="password"></li>
+                        <li><input class="submit-button"  type="submit" value="RECOVER"></li>
+                    </ul>
+                </form>
 
-             </div>
-
-             <div class="right-side">
-                 <form action="" method="get">
-                     <ul>
-                         <li><input class="input" type="text" placeholder="name" name="name"> </li>
-                         <li><input class="input" type="password" placeholder="password" name="password"></li>
-                         <li><input class="submit-button"  type="submit" value="LOGIN"></li>
-                     </ul>
-
-             </div>
+            </div>
         </div>
     </div>
+</div>
