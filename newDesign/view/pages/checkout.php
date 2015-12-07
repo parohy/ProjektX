7db8d6c9a38b2d05bdb6673e81a04594424fd00b
garysil <gergely.siladi@akademiasovy.sk>
2015-12-07 13:30:23 +0100

    Checkout page fixed.

--- newDesign/view/pages/checkout.php
+++ newDesign/view/pages/checkout.php
@@ -5,34 +5,36 @@
     <div class="title-page">
         <h1>YOUR INFO</h1>
     </div>
-    <div>
-        <div class="leftside">
-            <ul class="nav-bar">
-                         <li><a class="formular-link">NAME</a></li>
-                         <li><a class="formular-link">SURNAME</a></li>
-                         <li><a class="formular-link">EMAIL</a></li>
-                         <li><a class="formular-link">PHONE</a></li>
-                         <li><a class="formular-link">ADDRESS</a></li>
-                         <li><a class="formular-link">CITY</a></li>
-                         <li><a class="formular-link">PSC</a></li> 
-            </ul>
-            <form action="" method="get">
-                <ul class="text-bar">
-                    <li><input class="input" type="text" name="name"></li>
-                    <li><input class="input" type="text" name="surname"></li>
-                    <li><input class="input" type="text" name="email"></li>
-                    <li><input class="input" type="text" name="phone"></li>
-                    <li><input class="input" type="text" name="address"></li>
-                    <li><input class="input" type="text" name="city"><li>
-                    <li><input class="input" type="text" name="psc"></li>
-                </ul>
-            </form>    
-        </div>
-        <div class="rightside">
-            <div class="subtotal">Subtotal (x items)</div>
-            <div class="sum">199.99 EUR</div>
-        </div>   
-        
-    </div>
+    <div class="checkout-body">
+		<div class="leftside">
+		    <ul class="nav-bar">
+			 <li><a class="formular-link">NAME</a></li>
+			 <li><a class="formular-link">SURNAME</a></li>
+			 <li><a class="formular-link">EMAIL</a></li>
+			 <li><a class="formular-link">PHONE</a></li>
+			 <li><a class="formular-link">ADDRESS</a></li>
+			 <li><a class="formular-link">CITY</a></li>
+			 <li><a class="formular-link">PSC</a></li> 
+		    </ul>
+                    <form class="form-inp" action="" method="get">
+			<ul class="text-bar">
+                            <li><input class="input" type="text" name="name"></li>
+                            <li><input class="input" type="text" name="surname"></li>
+                            <li><input class="input" type="text" name="email"></li>
+                            <li><input class="input" type="text" name="phone"></li>
+                            <li><input class="input" type="text" name="address"></li>
+                            <li><input class="input" type="text" name="city"><li>
+                            <li><input class="input" type="text" name="psc"></li>
+			</ul>
+		    </form>    
+		</div>
+		<div class="rightside">
+                    <div class="subtotal">Subtotal (x items)</div>
+                    <div class="sum">199.99 EUR</div>
+                    <input type="button" class="order-button" value="Place order">
+		</div>
+    </div>    
+     
+    
 </div>
 
