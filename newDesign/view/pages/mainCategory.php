2194df3306cdc1a712d8b867cedbbbf02db1d540
Matúš Kačmár <matus.kacmar@akademiasovy.sk>
2015-12-08 22:50:10 +0100

    ciastocne dorobeny sorting form

--- newDesign/view/pages/mainCategory.php
+++ newDesign/view/pages/mainCategory.php
@@ -5,3 +5,10 @@
  * Date: 7. 12. 2015
  * Time: 14:23
  */
+?>
+<link rel="stylesheet" type="text/css" href="libraries/css/categories.css">
+<?php
+include (ROOT . 'view/pages/sortingTable.php');
+?>
+<div class="product-display">
+</div>
