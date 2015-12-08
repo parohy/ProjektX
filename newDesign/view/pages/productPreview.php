a821e73e6c79ab1f7739d2fb1be8a7de513be6ba
dominikkolesar <dominik.kolesar@akademiasovy.sk>
2015-12-09 00:18:55 +0100

    productPage

--- newDesign/view/pages/productPreview.php
+++ newDesign/view/pages/productPreview.php
@@ -5,3 +5,63 @@
  * Date: 8. 12. 2015
  * Time: 11:51
  */
+?>
+
+<link rel="stylesheet" type="text/css" href="productPreview.css">
+
+<div id="product-prewiew">
+
+    <div class="product-name">
+        <h1>PRODUCT NAME</h1>
+    </div>
+
+    <div id="about-product" class="group">
+
+        <div class="product-slider">
+            slider
+        </div>
+
+        <div id="product-info" class="group">
+
+            <div class="product-ranking">
+            hviezdicky
+            </div>
+
+            <div class="product-brand">
+            SONY
+            </div>
+
+            <span class="short-describtion">
+            Stream and browse easily with the BDP-S3500, a Wi-Fi® optimized Blu-ray Disc™ player that
+                plays online...
+            </span>
+
+            <form class="cost-form" action="" method="get">
+                <span class="cost">
+                    200 EUR
+                </span>
+                <div class="submit-input">
+                <input type="submit" value="BUY" name="BUY">
+                </div>
+            </form>
+        </div>
+
+
+        </div>
+
+
+    <div class="description-title">
+        <h2> DESCRIPTION</h2>
+
+    </div>
+
+    <div class="description-text">
+        As one of driving forces behind the Blu-ray format, Sony may have produced more Blu-ray players than anyone else.
+        Every year we see many new models from them as high-end features trickle down the line, and new features appear.
+        This trickle-down effect leads to every player being a better value than the year before, and to the higher-end players becoming even more niche products.
+
+    </div>
+
+
+
+</div>
