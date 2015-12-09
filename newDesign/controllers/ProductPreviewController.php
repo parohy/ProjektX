51f4da2baf8cdb3ff337a3015b96f87a113a3197
dominikkolesar <dominik.kolesar@akademiasovy.sk>
2015-12-09 13:57:48 +0100

    a

---
+++ newDesign/controllers/ProductPreviewController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matus
+ * Date: 9. 12. 2015
+ * Time: 8:12
+ */
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= 'ProjektX/newDesign/';
+include_once ($path.'config.php');
+include_once (ROOT.'API/Database.php');
+
+class ProductPreviewController {
+
+    private $handlerDB;
+
+    public function __construct() {
+        $this->handlerDB = new DBHandler();
+    }
+
+    public function getProduct($productid) {
+        $this->handlerDB->beginTransaction();
+        $this->handlerDB->query('SELECT * FROM products WHERE productid=:id');
+        $this->handlerDB->bind(':id',$productid);
+        $results = $this->handlerDB->singleRecord();
+        $this->handlerDB->endTransaction();
+
+        return $results;
+    }
+}
