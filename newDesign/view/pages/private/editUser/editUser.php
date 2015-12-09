0faa0c6791dcca8c940b677e5644d71b07a9be4e
dominikkolesar <dominik.kolesar@akademiasovy.sk>
2015-12-09 11:04:53 +0100

    formular na upravu userov

--- newDesign/view/pages/private/editUser/editUser.php
+++ newDesign/view/pages/private/editUser/editUser.php
@@ -1,9 +1,58 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Matus
+ * User: Matus ,Dominik
  * Date: 8. 12. 2015
  * Time: 22:04
  */
 
-// DO EDIT USER
+// DO EDIT USER ?>
+<link rel="stylesheet" href="libraries/css/editUser.css">
+
+<form id="profile" action="" method="POST">
+
+    <fieldset id="edit-profile">
+        <legend>EDIT PROFILE</legend>
+        <ul>
+            <li>
+
+                <input class="edit-input" type="text" name="name" value="" placeholder="NAME">
+            </li>
+
+            <li>
+
+                <input class="edit-input" type="text" name="surename" value="" placeholder="SURNAME">
+            </li>
+
+            <li>
+
+                <input class="edit-input" type="email" name="mail" value="" placeholder="EMAIL">
+            </li>
+
+
+            <li>
+
+                <input class="edit-input" type="text" name="mobile" value="" placeholder="MOBILE PHONE">
+            </li>
+
+            <li>
+
+                <input class="edit-input" type="text" name="address"value="" placeholder="ADRESS">
+            </li>
+
+            <li>
+
+                <input class="edit-input"  type="text" name="city"  value="" placeholder="CITY">
+            </li>
+
+            <li>
+
+                <input class="edit-input" type="text" name="postcode" value="" placeholder="POSTCODE">
+            </li>
+
+            <li>
+                <input id="edit-profile-submit" type="submit" value="SAVE CHANGE">
+            </li>
+        </ul>
+    </fieldset>
+</form>
