bc16cf2a22e8b53cee609059fc132bcaac0e8586
peter-varholak <peter.varholak@akademiasovy.sk>
2016-01-05 13:10:44 +0100

    fixed checkout.php

--- view/pages/checkout.php
+++ view/pages/checkout.php
@@ -8,34 +8,31 @@
     <div class="checkout-body">
 		<div class="leftside">
 		    <ul class="nav-bar">
-			 <li><a class="formular-link">NAME</a></li>
-			 <li><a class="formular-link">SURNAME</a></li>
-			 <li><a class="formular-link">EMAIL</a></li>
-			 <li><a class="formular-link">PHONE</a></li>
-			 <li><a class="formular-link">ADDRESS</a></li>
-			 <li><a class="formular-link">CITY</a></li>
-			 <li><a class="formular-link">PSC</a></li> 
+    			<li><a class="formular-link">NAME</a></li>
+    			<li><a class="formular-link">SURNAME</a></li>
+    			<li><a class="formular-link">EMAIL</a></li>
+    			<li><a class="formular-link">PHONE</a></li>
+    			<li><a class="formular-link">ADDRESS</a></li>
+    			<li><a class="formular-link">CITY</a></li>
+    			<li><a class="formular-link">PSC</a></li> 
 		    </ul>
-                    <form class="form-inp" action="" method="get">
-			<ul class="text-bar">
-                            <li><input class="input" type="text" name="name"></li>
-                            <li><input class="input" type="text" name="surname"></li>
-                            <li><input class="input" type="text" name="email"></li>
-                            <li><input class="input" type="text" name="phone"></li>
-                            <li><input class="input" type="text" name="address"></li>
-                            <li><input class="input" type="text" name="city"><li>
-                            <li><input class="input" type="text" name="psc"></li>
-			</ul>
+            <form class="form-inp" action="" method="get">
+    			<ul class="text-bar">
+                    <li><input class="input" type="text" name="name"></li>
+                    <li><input class="input" type="text" name="surname"></li>
+                    <li><input class="input" type="text" name="email"></li>
+                    <li><input class="input" type="text" name="phone"></li>
+                    <li><input class="input" type="text" name="address"></li>
+                    <li><input class="input" type="text" name="city"><li>
+                    <li><input class="input" type="text" name="psc"></li>
+    			</ul>
 		    </form>    
 		</div>
 		<div class="rightside">
-                    <div class="subtotal">Subtotal (x items)</div>
-                    <div class="sum">XX.XX EUR</div>
-                    <input type="button" class="order-button" value="Place order">
-                    <div class="querylogin">Do you want to <a class="boldlogin">login</a>?</div>
-                </div>
+            <div class="subtotal">Subtotal (x items)</div>
+            <div class="sum">XX.XX EUR</div>
+            <input type="button" class="order-button" value="Place order">
+            <div class="querylogin">Do you want to <a class="boldlogin">login</a>?</div>
+        </div>
     </div>    
-     
-    
 </div>
-
