989e93de6f31a30c7a550eeef21b41f10d785247
Tomáš Paronai <tomas.parok@gmail.com>
2016-01-07 17:11:43 +0100

    thankyou page checkout autofill

---
+++ view/pages/endline.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * @author Tomas Paronai
+ * */
+
+?>
+
+<link rel="stylesheet" type="text/css" href="libraries/css/endline.css">
+
+<h1 class="message">Thank you for purchasing our products.</h1>
+<h3 class="links">Download order <a id="pdf-link" href="">pdf</a> or view order <a id="order-link" href="">here</a>.</h3>
