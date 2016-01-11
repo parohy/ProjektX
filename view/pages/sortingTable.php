bb5c9875833c9658df4a297804613c8999e0041e
Matúš Kačmár <matus.kacmar@akademiasovy.sk>
2016-01-11 08:01:03 +0100

    Rating sort removed

--- view/pages/sortingTable.php
+++ view/pages/sortingTable.php
@@ -29,16 +29,5 @@
             <li><input type="checkbox" name="hp"> HP</li>
             <li><input type="checkbox" name="samsung"> SAMSUNG</li>
         </ul>
-    </form>
-
-    <form action="" method="GET">
-        <ul>
-            <li><span class="table-header">RATING</span></li>
-            <li><input type="checkbox" name="apple"> & UP</li>
-            <li><input type="checkbox" name="asus"> & UP</li>
-            <li><input type="checkbox" name="dell"> & UP</li>
-            <li><input type="checkbox" name="hp"> & UP</li>
-            <li><input type="checkbox" name="samsung"> & UP</li>
-        </ul>
-    </form>
+    </form> 
 </div>
