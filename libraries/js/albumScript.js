2038f17902754762d513a644b714b0584643cb45
Tomas-Paronai <tomas.paronai@akademiasovy.sk>
2016-01-12 11:48:18 +0100

    alert disabled, sorry

--- libraries/js/albumScript.js
+++ libraries/js/albumScript.js
@@ -6,5 +6,5 @@ function changePic(index,id){
 	var path = 'libraries/img/products/'+id+'/'+id+index+'.jpg';
 	var image = document.getElementById('propic');
 	image.src = path;
-	alert(path);
+	//alert(path);
 }
