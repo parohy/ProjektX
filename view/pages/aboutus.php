b1302fc7da59d2b3c4e65b4067b1de1efbceb487
Matúš Kačmár <matus.kacmar@akademiasovy.sk>
2016-01-17 16:44:07 +0100

    About us concept

---
+++ view/pages/aboutus.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matus
+ * Date: 16. 1. 2016
+ * Time: 18:11
+ */
+?>
+<link rel="stylesheet" type="text/css" href="libraries/css/aboutus.css">
+<div class="aboutUs">
+    <div class="left">
+        <ul>
+            <li><h1>ABOUT US</h1></li>
+            <li>Adress:<span class="info">Dneperska 1, Kosice, Slovakia</span></li>
+            <li>Phone number:<span class="info">+421 902 095 588</span></li>
+            <li>E-mail:<span class="info"><a href="mailto:viashopsk@gmail.com">viashopsk@gmail.com</a></span></li>
+            <li><b>Working hours:</b></li>
+            <li>MON-FRI:<span class="info">8:00-16:00</span></li>
+            <li>SAT:<span class="info">8:00-12:00</span></li>
+            <li>SUN:<span class="info">CLOSED</span></li>
+            <li><b>BIO:</b>
+                <article>
+                    <p>
+                    We are E-shop specialized in sales of electronical gadgets and products.
+                    </p>
+                </article>
+            </li>
+        </ul>
+    </div>
+    <div class="right">
+        <h4>Map:</h4>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1566.1482497049863!2d21.28632480844072!3d48.6875710950816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473edf8c1176ba7d%3A0x84e657f5c09ebade!2zU8O6a3JvbW7DqSBHeW1uw6F6aXVt!5e0!3m2!1ssk!2ssk!4v1452964538262" width="600" height="450" frameborder="0" style="border:0" allowfullscreen class="map"></iframe>
+    </div>
+</div>
