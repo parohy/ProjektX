7ee1388adaf11bbca360dbb50337209a1be5c973
peter-varholak <peter.varholak@akademiasovy.sk>
2016-01-24 20:14:36 +0100

    fixnute scalovanie product preview obrazkov

--- view/pages/productPreview.php
+++ view/pages/productPreview.php
@@ -16,27 +16,58 @@ $product = $productController->getProduct($_GET['product']);
 ?>
 
 <link rel="stylesheet" type="text/css" href="libraries/css/productPreview.css">
+<script>
+$(document).ready(function()
+{
+    $(".image-album").each(function(){
+        adjustOneThumbnail($(this));
+    });
 
-<div id="product-prewiew">
-    
+    function adjustOneThumbnail(img)
+    {
+        var maxWidth = img.parent().width() - 5;
+        var maxHeight = img.parent().height() - 5;
+
+        var ratio;
+        var width = img.width();
+        var height = img.height();
+
+        ratio = maxWidth / width;
+        img.css("width", maxWidth);
+        img.css("height", height * ratio);
+        height *= ratio;
+        width *= ratio;
+
+        if(height > maxHeight){
+            ratio = maxHeight / height;
+            img.css("height", maxHeight);
+            img.css("width", width * ratio);
+            height *= ratio;
+            width *= ratio;
+        }
+
+        var marginTop = ((maxHeight+5) - height) / 2;
+        var marginLeft = ((maxWidth+5) - width) / 2;
+        img.css({"margin-top":marginTop+"px","margin-left":marginLeft+"px"});
+        img.removeClass("notLoaded");
+    }
+});
+</script>
+<div id="product-prewiew">    
     <div class="product-slider">
             <?php
-
-            $scaling = new ImageScaling();
-            if(isset($_GET['index'])){
-            	$size = $scaling->productPreviewImageIndex($product['productid'], $_GET['index']);
-            	$imgName = $product['productid'] . $_GET['index'] . '.jpg';
-            	echo '<img id="propic" src="libraries/img/products/' . $product['productid'] . '/' . $imgName . '" width="' . $size[0] . '" height="' . $size[1] . '">';
-            }
-            else{
-            	$size = $scaling->productPreviewImage($product['productid']);
-            	echo '<img id="propic" src="libraries/img/products/' . $product['productid'] . '/' . $product['productid'] . 'a.jpg" width="' . $size[0] . '" height="' . $size[1] . '">';
-            }            
-            
+                $scaling = new ImageScaling();
+                if(isset($_GET['index'])){
+                	$size = $scaling->productPreviewImageIndex($product['productid'], $_GET['index']);
+                	$imgName = $product['productid'] . $_GET['index'] . '.jpg';
+                	echo '<img id="propic" src="libraries/img/products/' . $product['productid'] . '/' . $imgName . '" width="' . $size[0] . '" height="' . $size[1] . '">';
+                }
+                else{
+                	$size = $scaling->productPreviewImage($product['productid']);
+                	echo '<img id="propic" src="libraries/img/products/' . $product['productid'] . '/' . $product['productid'] . 'a.jpg" width="' . $size[0] . '" height="' . $size[1] . '">';
+                }            
             ?>
     </div>
-	
-	
     <div id="about-product" class="group">
         
 		<div class="product-name">
@@ -45,13 +76,13 @@ $product = $productController->getProduct($_GET['product']);
     
         <div class="aftername1">
                   
-        <div class="product-brand">
-            <?php
-                $brand = $productController->getProductBrand($product['brandid']);
-                echo strtoupper($brand['name']);
+            <div class="product-brand">
+                <?php
+                    $brand = $productController->getProductBrand($product['brandid']);
+                    echo strtoupper($brand['name']);
                 ?>
             </div>
-            
+                
             <div class="product-ranking">
             <?php
                 if($product['numofratings'] > 0) {
@@ -72,8 +103,10 @@ $product = $productController->getProduct($_GET['product']);
                             }
                         }
                     }
-                    else if($_COOKIE[$id] == "false") {
-                        for($i = 1; $i <= 5; $i++) {
+                    else if($_COOKIE[$id] == "false")
+                    {
+                        for($i = 1; $i <= 5; $i++)
+                        {
                             if($i <= $rating) {
                                 echo '<a href="?page=productPreview&product=' . $id . '&rating=' . $i .'"><img src="libraries/img/star-selected.png"></a>';
                             }
@@ -83,7 +116,8 @@ $product = $productController->getProduct($_GET['product']);
                         }
                     }
                 } else {
-                    for($i = 1; $i <= 5; $i++) {
+                    for($i = 1; $i <= 5; $i++)
+                    {
                         if($i <= $rating) {
                             echo '<a href="?page=productPreview&product=' . $id . '&rating=' . $i .'"><img src="libraries/img/star-selected.png"></a>';
                         }
@@ -95,60 +129,47 @@ $product = $productController->getProduct($_GET['product']);
             ?>
             </div>
 
-            <!-- album of pictures
-        @author Tomas Paronai-->
-        <script src="libraries/js/albumScript.js"></script>
-        <div class="pictable">
-        <table>
-        	<tr>
-        		<?php
-        			$alpha = range('a','z');
-        			$index = 0;
-        			while(file_exists('libraries/img/products/' . $product['productid'] . '/' . $product['productid'] . $alpha[$index] . '.jpg')){
-        				$localPath = 'libraries/img/products/' . $product['productid'] . '/' . $product['productid'] . $alpha[$index] . '.jpg';
-        				$width = $height = 100;
-        				$alt = $product['productid'] . $alpha[$index] . '.jpg';
-        				//echo '<td><a href="?page=productPreview&product=' . $product['productid'] . '&index='. $alpha[$index] . '" onclick="changePic('.$alpha[$index].')"><img class="image-album" src="' . $localPath . '" alt="' . $alt . '" width="' . $width . '" height="' . $height . '"></a></td>';
-        				echo '<td><img class="image-album" src="' . $localPath . '" alt="' . $alt . '" width="' . $width . '" height="' . $height . '" onclick="changePic(\''.$alpha[$index].'\',\''.$product['productid'].'\')"></td>';
-        				$index++;
-        			}
-        		?>
-        	</tr>
-        </table>
-        </div>
+                <!-- album of pictures
+            @author Tomas Paronai-->
+            <script src="libraries/js/albumScript.js"></script>
+            <div class="pictable">
+                <table>
+                	<tr>
+                		<?php
+                			$alpha = range('a','z');
+                			$index = 0;
+                			while(file_exists('libraries/img/products/' . $product['productid'] . '/' . $product['productid'] . $alpha[$index] . '.jpg')){
+                				$localPath = 'libraries/img/products/' . $product['productid'] . '/' . $product['productid'] . $alpha[$index] . '.jpg';
+                				$alt = $product['productid'] . $alpha[$index] . '.jpg';
+                				//echo '<td><a href="?page=productPreview&product=' . $product['productid'] . '&index='. $alpha[$index] . '" onclick="changePic('.$alpha[$index].')"><img class="image-album" src="' . $localPath . '" alt="' . $alt . '" width="' . $width . '" height="' . $height . '"></a></td>';
+                				echo '<td><img class="image-album" src="' . $localPath . '" alt="' . $alt . '" width="100" onclick="changePic(\''.$alpha[$index].'\',\''.$product['productid'].'\')"></td>';
+                				$index++;
+                			}
+                		?>
+                	</tr>
+                </table>
+            </div>
 
-            <!--<span class="short-describtion">
-            <?php echo substr($product['description'],0,200);?>
-            </span>-->
+                <!--<span class="short-describtion">
+                <?php echo substr($product['description'],0,200);?>
+                </span>-->
 
             <form class="cost-form" action="controllers/addToCart.php?productid=<?php echo $product['productid'] ?>&name=<?php echo $product['name'] ?>&price=<?php echo $product['price'] ?>" method="POST">
                 <span class="cost">
                     <?php echo $product['price'] . ' €';?>
                 </span>
                 <div class="submit-input">
-                <input type="submit" value="BUY" name="BUY">
+                    <input type="submit" value="BUY" name="BUY">
                 </div>
-            </form>
-        
-		</div>
-		<div class="desc">
-        <div class="description-title">
-            <p> DESCRIPTION</p>
-            
+            </form>            
+    	</div>
+    	<div class="desc">
+            <div class="description-title">
+                <p> DESCRIPTION</p>                
+            </div>            	
+            <div class="description-text">
+                <?php echo $product['description'];?>
+            </div>       
         </div>
-        	
-        <div class="description-text">
-            <?php echo $product['description'];?>
-
-        </div>
-        
-	
-        
     </div>
-
-
-    </div>
-		
-    
-
 </div>
