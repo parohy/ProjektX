90fe2df3d43fec2b8134fef0ca1161624872e605
peter-varholak <peter.varholak@akademiasovy.sk>
2016-01-24 14:32:49 +0100

    labely na formy

--- view/pages/reg-acc.php
+++ view/pages/reg-acc.php
@@ -41,13 +41,28 @@ $_SESSION['captcha'] = $a + $b;
 <!--  <div id="all-tab">-->
 <!-- <div id="tab1" class="tab-content">-->
 <form class="form" action='API/Relog.php?register=registration' onSubmit="return isPassEqual()" method="POST">
-    <ul>
-        <li><input class="input" type="text" placeholder="name" name="name" value="<?php echo $name;?>"> </li>
-        <li><input class="input" type="text" placeholder="surname" name="last-name" value="<?php echo $surname;?>"> </li>
-        <li><input class="input" type="email" placeholder="email" name="mail" value="<?php echo $email;?>"> </li>
+    <ul class="registrationForm">
+        <li>
+            <div class="inputName">Name: </div>
+            <input class="input" type="text" placeholder="name" name="name" value="<?php echo $name;?>">
+        </li>
+        <li>
+            <div class="inputName">Surname: </div>
+            <input class="input" type="text" placeholder="surname" name="last-name" value="<?php echo $surname;?>">
+        </li>
+        <li>
+            <div class="inputName">E-mail: </div>
+            <input class="input" type="email" placeholder="email" name="mail" value="<?php echo $email;?>">
+        </li>
         <li><span id="pass-err"></span></li>
-        <li><input class="input" type="password" placeholder="password" name="password" id="pass1"></li>
-        <li><input class="input" type="password" placeholder="password" name="password" id="pass2"></li>
+        <li>
+            <div class="inputName">Password: </div>
+            <input class="input" type="password" placeholder="password" name="password" id="pass1">
+        </li>
+        <li>
+            <div class="inputName">Retype password: </div>
+            <input class="input" type="password" placeholder="password" name="password" id="pass2">
+        </li>
         <li><?php echo $a.' + '.$b.' = ';?><input class="input" type="number" placeholder="I am not a robot" name="captcha" id="captcha"></li>
         <li><input type="submit" class="submit-button" value="REGISTER"></li>
     </ul>
