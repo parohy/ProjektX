b0057ca1eeaf748200ce961cb15bddf6309eda99
Matúš Kačmár <matus.kacmar@akademiasovy.sk>
2016-02-05 22:01:47 +0100

    social profiles working

--- view/includes/footer.php
+++ view/includes/footer.php
@@ -14,14 +14,14 @@
         <li ><a target="_blank" href="https://goo.gl/maps/dL9vTMNTK9w">Dneperska 1, Kosice, Slovakia</a></li>
         <li>+421 902 095 588</li>
 
-        <li><a class="mail" href="mailto:viatech@gmail.com">viatech@gmail.com</a></li>
+        <li><a class="mail" href="mailto:viatech@gmail.com">viatechcassomedia@gmail.com</a></li>
         <li><a href="?page=faq">FAQ</a>/<a href="?page=aboutus">About us</a></li>
     </ul>
     <div class="social-icons">
         <ul>
-        <li><a href="https://www.facebook.com"> <i class="fa fa-facebook "></i> </a></li>
-        <li><a href="https://plus.google.com/"> <i class="fa fa-google-plus "></i></a></li>
+        <li><a href="https://www.facebook.com/viatecheshop/"> <i class="fa fa-facebook "></i> </a></li>
+        <li><a href="https://plus.google.com/u/0/100194044086960098565/posts/p/pub?hl=en"> <i class="fa fa-google-plus "></i></a></li>
 
-        <li><a class="twiter" href="https://twitter.com/"> <i class="fa fa-twitter "></i></a></li>
+        <li><a class="twiter" href="https://twitter.com/viatechcassomed"> <i class="fa fa-twitter "></i></a></li>
         </ul>
     </div>
