8ffc1b1b9718d3fa4e6f9c9707182805321806cd
Matúš Kačmár <matus.kacmar@akademiasovy.sk>
2016-02-15 20:40:23 +0100

    social pages opening in new tab in browser

--- view/includes/footer.php
+++ view/includes/footer.php
@@ -19,9 +19,9 @@
     </ul>
     <div class="social-icons">
         <ul>
-        <li><a href="https://www.facebook.com/viatecheshop/"> <i class="fa fa-facebook "></i> </a></li>
-        <li><a href="https://plus.google.com/u/0/100194044086960098565/posts/p/pub?hl=en"> <i class="fa fa-google-plus "></i></a></li>
+        <li><a href="https://www.facebook.com/viatecheshop/" target="_blank"> <i class="fa fa-facebook "></i> </a></li>
+        <li><a href="https://plus.google.com/u/0/100194044086960098565/posts/p/pub?hl=en" target="_blank"> <i class="fa fa-google-plus "></i></a></li>
 
-        <li><a class="twiter" href="https://twitter.com/viatechcassomed"> <i class="fa fa-twitter "></i></a></li>
+        <li><a class="twiter" href="https://twitter.com/viatechcassomed" target="_blank"> <i class="fa fa-twitter "></i></a></li>
         </ul>
     </div>
