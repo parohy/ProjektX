a30cba4c171e70ee59b399ad2f39e8acdede3777
Matúš Kačmár <matus.kacmar@akademiasovy.sk>
2016-02-19 00:31:45 +0100

    Back to home page button

--- view/pages/endline.php
+++ view/pages/endline.php
@@ -13,8 +13,9 @@ if(isset($_SESSION['filepath'])){
 
 <link rel="stylesheet" type="text/css" href="libraries/css/endline.css">
 
-<h1 class="message">Thank you for purchasing our products.</h1>
+<h1 class="endlineMessage">Thank you for purchasing our products.</h1>
 <h3 class="links">Download order <a id="pdf-link" href="<?php echo $topdf;?>">pdf</a> or view order <a id="order-link" href="">here</a>.</h3>
+<a href="?page=main-page" class="backToHome">BACK TO HOME PAGE</a>
 
 <?php
 if(isset($_SESSION['cart'])) {
