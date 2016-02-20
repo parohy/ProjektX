7e99ab5c576c01d76e81edaa192169fdf59e75a1
Tomáš Paronai <tomas.parok@gmail.com>
2016-02-20 12:51:14 +0100

    icons, shipped (SQL bullshit error)

---
+++ view/pages/private/editOrder/editOrder.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tomas
+ * Date: 2/20/2016
+ * Time: 12:25 PM
+ */
+
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= 'ProjektX/';
+
+include_once($path.'API/Orders.php');
+
+$order = null;
+if(isset($_GET['orderid'])){
+    $order = new Order($_GET['orderid']);
+}
+
+if(isset($_GET['edit'])){
+    if($_GET['edit'] == 'ship'){
+        if($order->shipped == 0){
+            $order->shipped = 1;
+        }
+        else{
+            $order->shipped = 0;
+        }
+        $order->save();
+    }
+    else{
+
+    }
+}
+
+if(headers_sent()){
+    die('Redirect failed. Please click on <a href="'.$_SERVER['HTTP_REFERER'].'">this</a> to try again.');
+}
+else{
+    exit(header('Location: ' . $_SERVER['HTTP_REFERER']));
+}
--- view/pages/private/orders.php
+++ view/pages/private/orders.php
@@ -20,13 +20,27 @@ else{
     $DBHandler->query('SELECT * FROM orders WHERE userid=:userid');
     $DBHandler->bind(':userid',$_SESSION['userid']);
 }
-$array = $DBHandler->resultSet();
+
+if(isset($_SESSION['searchRes'])){
+    $array = $_SESSION['searchRes'];
+    unset($_SESSION['searchRes']);
+}
+else{
+    $array = $DBHandler->resultSet();
+}
+
 
 
 echo '<ol class="items">';
 for($i = 0; $i < count($array); $i++){
     echo '<li>';
-    echo '<span class="orderinfo"><div style="width: 220px; display: inline-block">'.$array[$i]['address'].'</div><div style="width: 150px; display: inline-block">'.$array[$i]['city'].'</div><div style="width: 150px; display: inline-block">'.$array[$i]['postcode'].'</div></span>';
+    if($_SESSION['userrole'] != 1){
+        echo '<span class="orderinfo"><div style="width: 220px; display: inline-block">'.$array[$i]['name'].' '.$array[$i]['surname'].' '.$array[$i]['address'].'</div><div style="width: 150px; display: inline-block">'.$array[$i]['city'].'</div><div style="width: 150px; display: inline-block">'.$array[$i]['postcode'].'</div></span>';
+    }
+    else{
+        echo '<span class="orderinfo"><div style="width: 220px; display: inline-block">'.$array[$i]['address'].'</div><div style="width: 150px; display: inline-block">'.$array[$i]['city'].'</div><div style="width: 150px; display: inline-block">'.$array[$i]['postcode'].'</div></span>';
+
+    }
     echo '<span class="orderinfoprice"> '.$array[$i]['orderprice'].' eur</span>';
         if($array[$i]['shipped'] != '0'){
             echo '<span class="orderinfoship"> shipped </span>';
@@ -34,7 +48,9 @@ for($i = 0; $i < count($array); $i++){
         else{
             echo '<span class="orderinfoship"> queued </span>';
         }
-    echo '<a class="page-link" href="?page=accountSettings&profile=orderPreview&orderid='.$array[$i]['orderid'].'"><i class="fa fa-pencil-square-o fa-2x"></i></a>';
+    echo '<a class="page-link" href="?page=private/pageSettings&settings=orderPreview&orderid='.$array[$i]['orderid'].'"><i class="fa fa-search-plus fa-2x"></i></a>';
+    //echo '<a class="page-link" href="?page=private/pageSettings&settings=orderPreview&orderid='.$array[$i]['orderid'].'"><i class="fa fa-pencil-square-o fa-2x"></i></a>';
+    echo '<a class="page-link" href="?page=private/pageSettings&settings=editOrder/editOrder&orderid='.$array[$i]['orderid'].'&edit=ship"><i class="fa fa-plane fa-2x"></i></a>';
     echo '</li>';
 }
 echo '</ol>';
