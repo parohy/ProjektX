816b905d00dc79e63aed5d0052c54b76a3609c78
dominikkolesar <dominik.kolesar@akademiasovy.sk>
2016-02-22 12:27:04 +0100

    statistics labels

--- view/pages/private/statistics.php
+++ view/pages/private/statistics.php
@@ -5,3 +5,33 @@
  * Date: 8. 12. 2015
  * Time: 17:28
  */
+?>
+<link rel="stylesheet" type="text/css" href="libraries/css/admin-panel.css">
+
+<div class="statistics">
+        <table>
+            <tr>
+                <td>Products:</td>  <td></td>
+            </tr>
+
+            <tr>
+                <td>Registered users:</td>  <td></td>
+            </tr>
+
+            <tr>
+                <td>Orders shipped:</td>  <td></td>
+            </tr>
+
+            <tr>
+                <td>Orders not shipped:</td>  <td></td>
+            </tr>
+
+            <tr>
+                <td>Total revenue:</td>  <td></td>
+            </tr>
+
+            <tr>
+                <td>Page views:</td>  <td></td>
+            </tr>
+        </table>
+</div>
